118bb0373d24849b1ce110dafa61fe03a489a5fa
Eskender <eskenderbesrat@gmail.com>
2019-07-03 17:17:57 +0800

    Add scaling

--- preprocess.py
+++ preprocess.py
@@ -1,12 +1,64 @@
 from __future__ import print_function, division
 import os
 import torch
-import pandas as pd
 from skimage import io, transform
 import numpy as np
 import matplotlib.pyplot as plt
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
+
+
+class Rescale(object):
+	"""Rescale the image in a sample to a given size.
+
+	Args:
+	output_size (tuple or int): Desired output size. If tuple, output is
+	matched to output_size. If int, smaller of image edges is matched
+	to output_size keeping aspect ratio the same.
+	"""
+
+	def __init__(self, output_size):
+		assert isinstance(output_size, (int, tuple))
+		self.output_size = output_size
+
+	def __call__(self, sample):
+		image, labels = sample['image'], sample['labels']
+
+		h, w = image.shape[:2]
+		if isinstance(self.output_size, int):
+			if h > w:
+				new_h, new_w = self.output_size * h / w, self.output_size
+			else:
+				new_h, new_w = self.output_size, self.output_size * w / h
+		else:
+			new_h, new_w = self.output_size
+
+		new_h, new_w = int(new_h), int(new_w)
+
+		new_img = transform.resize(image, (new_h, new_w))
+		new_labels = np.zeros([labels.shape[0], new_h, new_w], dtype=labels.dtype)
+
+		for i in range(labels.shape[0]):
+			new_labels[i,:,:] = transform.resize(labels[i,:,:], (new_h, new_w))
+
+		return {'image': new_img, 'labels': new_labels}
+
+
+class ToTensor(object):
+	"""Convert ndarrays in sample to Tensors."""
+
+	def __call__(self, sample):
+		image, labels = sample['image'], sample['labels']
+
+		# swap color axis because
+		# numpy image: H x W x C
+		# torch image: C X H X W
+		image = image.transpose((2, 0, 1))
+		return {'image': torch.from_numpy(image),
+		'labels': torch.from_numpy(labels)}
+
+
+
 
 class ImageDataset(Dataset):
 	"""Image dataset."""
