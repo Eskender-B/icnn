b2f84b2d3eeafffd79f61a4fea00e4a4050aa0a7
Eskender <eskenderbesrat@gmail.com>
2019-07-04 15:28:04 +0800

    Rearrange some conv weights

--- model.py
+++ model.py
@@ -22,9 +22,10 @@ class ICNN(nn.Module):
 
 
 		# Interlinking convs
-		self.inter_convs1 = nn.ModuleList([nn.Conv2D(num_channel_orignal[r], num_channel_interlinked[r], self.kernel_size, padding=self.kernel_size//2) for r in range(self.num_rows)])
-		self.inter_convs2 = nn.ModuleList([nn.Conv2D(num_channel_orignal[r], num_channel_interlinked[r], self.kernel_size, padding=self.kernel_size//2) for r in range(self.num_rows)])
-		self.inter_convs2 = nn.ModuleList([nn.Conv2D(num_channel_orignal[r], num_channel_interlinked[r], self.kernel_size, padding=self.kernel_size//2) for r in range(self.num_rows)])
+		self.inter_convs_row0 = nn.ModuleList([nn.Conv2D(num_channel_orignal[0], num_channel_interlinked[0], self.kernel_size, padding=self.kernel_size//2) for i in range(self.num_interlink_layer)])
+		self.inter_convs_row1 = nn.ModuleList([nn.Conv2D(num_channel_orignal[1], num_channel_interlinked[1], self.kernel_size, padding=self.kernel_size//2) for i in range(self.num_interlink_layer)])
+		self.inter_convs_row2 = nn.ModuleList([nn.Conv2D(num_channel_orignal[2], num_channel_interlinked[2], self.kernel_size, padding=self.kernel_size//2) for i in range(self.num_interlink_layer)])
+		self.inter_convs_row3 = nn.ModuleList([nn.Conv2D(num_channel_orignal[3], num_channel_interlinked[3], self.kernel_size, padding=self.kernel_size//2) for i in range(self.num_interlink_layer)])
 
 
 		# Output convs
@@ -36,26 +37,32 @@ class ICNN(nn.Module):
 
 
 		# Last conv
-		self.last_conv = nn.Conv2D(self.L, 2*self.L+8,
+		self.last_conv1 = nn.Conv2D(self.L, 2*self.L+8,
+		 , self.kernel_size, padding=self.kernel_size//2)
+
+		self.last_conv2 = nn.Conv2D(self.L, self.L,
 		 , self.last_kernel_size, padding=self.last_kernel_size//2)
 
 
 
 	def forward(self, inp):
 
-		# Scale
+		# Scale, convolve and output rows of features maps
+		# After this: inps = feature maps of [row1, row2, row3, row4]
+		inps = [self.inp_convs[0](inp)]
+		for i in range(1, self.num_rows):
+			inps.append(self.inp_convs[i](F.max_pool2d(inp, kernel_size=self.kernel_size, stride=2**i, padding=self.kernel_size//2)))
 
-		# Rows of input
-		inps = 
 
 		# Step forward for each interlinking layer
 		for i in range(self.num_interlink_layer):
 
-			# Do interlinking
-
-			# Process each row 
+			# For each row
+			interlink
 			for j in range(num_rows):
-				pass
+				# Interlink
+
+				# Convolve
 
 		inps =
 
