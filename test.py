aade2e932ebf59202d23e6c6028dd76e5583c749
Eskender <eskenderbesrat@gmail.com>
2019-07-07 22:11:55 +0800

    Fix visualization

--- test.py
+++ test.py
@@ -9,6 +9,8 @@ import argparse
 import numpy as np
 from scipy import ndimage
 import matplotlib.pyplot as plt
+import torch.nn.functional as F
+import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--batch_size", default=25, type=int, help="Batch size to use during training.")
@@ -63,49 +65,42 @@ LOG_INFO('test loss = %.4f' % (test_loss))
 ###### See some images ######
 #colors = torch.Tensor([(255,255,255),	(255,0,0), (0,255,0), (0,0,255), (255,255,0), (0,255,255), (255,0,255),(192,192,192)]).to(device)
 
-def show_centroids(image, centroids1, centroids2):
+def show_centroids(image, centroids_pred, centroids_orig):
     """Show image with landmarks"""
+    h,w,c = image.shape
     plt.imshow(image)
-    plt.scatter(centroids[:, 0], centroids1[:, 1], s=10, marker='x', c='r')
-    plt.scatter(centroids[:, 0], centroids2[:, 1], s=10, marker='x', c='g')
+    plt.scatter(w/64*centroids_orig[:, 0], h/64*centroids_orig[:, 1], s=10, marker='x', c='r')
+    plt.scatter(w/64*centroids_pred[:, 0], h/64*centroids_pred[:, 1], s=10, marker='x', c='g')
     plt.pause(0.001)  # pause a bit so that plots are updated
 
 def calculate_centroids(tensor):
 	tensor = tensor.float()
 	n,l,h,w = tensor.shape
-	
 	indexs_y = torch.from_numpy(np.arange(h)).float().to(tensor.device)
 	indexs_x = torch.from_numpy(np.arange(w)).float().to(tensor.device)
-	
 	center_y = tensor.sum(3) * indexs_y.view(1,1,-1) 
 	center_y = center_y.sum(2, keepdim=True) / tensor.sum([2,3]).view(n,l,1)
-
 	center_x = tensor.sum(2) * indexs_x.view(1,1,-1)
 	center_x = center_x.sum(2, keepdim=True) / tensor.sum([2,3]).view(n,l,1)
-
 	return torch.cat([center_x, center_y], 2)
 
 n_test = 4
 indxs = np.random.randint(len(test_dataset), size=n_test)
-
 unresized_images = [unresized_dataset[i]['image'] for i in indxs]
 
 images = torch.stack([test_dataset[i]['image'] for i in indxs]).to(device)
 orig_labels = np.array([test_dataset[i]['labels'].numpy() for i in indxs])
-pred_labels = torch.softmax(model(images), 1)
-
-orig_centroids = calculate_centroids(torch.softmax(torch.from_numpy(orig_labels),1))	# get centroid and scale back
+orig_centroids = calculate_centroids(F.normalize(torch.from_numpy(orig_labels),1))
 orig_centroids = np.array( orig_centroids.detach().to('cpu').numpy(), np.int)
 
-centroids = 4 *calculate_centroids(pred_labels)	# get centroid and scale back
+pred_labels = F.normalize(model(images), 1)
+centroids = calculate_centroids(pred_labels)	
 centroids = np.array( centroids.detach().to('cpu').numpy(), np.int)
 
-
+if os.path.exists('res/'):
+	os.rmdir('res/')
+os.mkdir('res')
 for i in range(n_test):
-	ax = plt.subplot(2, 2, i + 1)
-	plt.tight_layout()
-	ax.set_title('Sample #{}'.format(i))
-	ax.axis('off')
 	show_centroids(unresized_images[i], centroids[i], orig_centroids[i])
-
-plt.savefig('test_outputs.jpg')
+	plt.savefig('res/stage1_image%d.jpg'%i)
+	plt.close()
