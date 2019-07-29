Fix combine_results, model; Add scaling

end2end.py
@@ -231,13 +231,11 @@ def combine_results(pred_labels, orig, centroids):
 
 
     # Mouth parts
-    y, x = centroids[i][5]
+    y, x = np.array(np.array(centroids[i][5:8], dtype=np.float).mean(0), np.long)
     pred_mask[i,y-40:y+40,x-40:x+40,:] += upper_lip[i].unsqueeze(-1) * colors[5].view(1,1,3)
 
-    y, x = centroids[i][6]
     pred_mask[i,y-40:y+40,x-40:x+40,:] += inner_mouth[i].unsqueeze(-1) * colors[6].view(1,1,3)
 
-    y, x = centroids[i][7]
     pred_mask[i,y-40:y+40,x-40:x+40,:] += lower_lip[i].unsqueeze(-1) * colors[7].view(1,1,3)
 
   alpha = 0.1

model.py
@@ -69,7 +69,7 @@ class ICNN(nn.Module):
 		scaled_inp = inp
 		inps = [torch.tanh(self.inp_bnorm[0](self.inp_convs[0](self.initial_bnorm[0](scaled_inp))))]
 		for i in range(1, self.num_rows):
-			scaled_inp = F.avg_pool2d(scaled_inp, kernel_size=3, stride=self.sf, padding=1)
+			scaled_inp = F.avg_pool2d(scaled_inp, kernel_size=2, stride=self.sf)
 			inps.append(torch.tanh(self.inp_bnorm[i](self.inp_convs[i](self.initial_bnorm[i](scaled_inp)))))
 
 

preprocess.py
@@ -23,12 +23,28 @@ class DataArg(object):
 		scale = np.random.random()*(1.1-0.9) + 0.9
 
 		h,w,c = image.shape
-		h,w = int(scale*h), int(scale*w)
+		new_h, new_w = int(scale*h), int(scale*w)
 		labels = labels.transpose(1,2,0) # process all labels in one go
 
 		## Scale
-		#image = transform.resize(image, (h,w))
-		#labels = transform.resize(labels, (h,w))
+		"""
+		image = transform.resize(image, (new_h,new_w))
+		labels = transform.resize(labels, (new_h,new_w))
+
+		if new_h - h > 0:
+			image = image[:(h-new_h),:,:]
+			labels = labels[:(h-new_h),:,:]
+		elif new_h - h < 0:
+			image = np.pad(image, ((0,(h-new_h)), (0,0), (0,0)), mode='constant')
+			labels = np.pad(labels, ((0,(h-new_h)), (0,0), (0,0)), mode='constant')
+
+		if new_w - w > 0:
+			image = image[:,:(w-new_w),:]
+			labels = labels[:,:(w-new_w),:]
+		elif new_w - w < 0:
+			image = np.pad(image, ((0,0), (0,(w-new_w)), (0,0)), mode='constant')
+			labels = np.pad(labels, ((0,0), (0,(w-new_w)), (0,0)), mode='constant')
+		"""
 
 		## Rotate
 		image = transform.rotate(image, angle)